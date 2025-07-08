9451bbe7e06dcacdba13750216abf0eeb38ba512
owl352 <kokosinca123@gmail.com>
2025-07-08 10:06:00 +0300

    fix order clauses

--- src/document/verify_proof.rs
+++ src/document/verify_proof.rs
@@ -147,7 +147,23 @@ pub fn verify_document_proof(
     let internal_clauses = InternalClauses::extract_from_clauses(query_where_clauses).map_err(JsError::from)?;
 
     // Parse order by
-    let order_by_map = parse_order_by(order_by)?;
+    let mut order_by_map = IndexMap::new();
+    
+    let js_order_by_array = Array::from(&order_by);
+    
+    for js_order_by_value in js_order_by_array.iter() {
+        let order_by_array = Array::from(&js_order_by_value);
+        
+        let field = order_by_array.get(0).as_string().unwrap();
+        let ascending = match order_by_array.get(1).as_string().unwrap().as_str() { 
+            "desc" => false,
+            _ => true,
+        };
+        
+        let order_clause = OrderClause{ field: field.clone(), ascending };
+        
+        order_by_map.insert(field, order_clause);
+    }
 
     // Parse start_at
     let start_at_bytes = start_at.map(|arr| {
@@ -195,212 +211,4 @@ pub fn verify_document_proof(
         root_hash: root_hash.to_vec(),
         documents: js_array,
     })
-}
-
-fn parse_internal_clauses(where_clauses: &JsValue) -> Result<InternalClauses, JsValue> {
-    if where_clauses.is_null() || where_clauses.is_undefined() {
-        return Ok(InternalClauses::default());
-    }
-
-    let obj: Object = where_clauses.clone().dyn_into().map_err(|_| {
-        format_error(
-            ErrorCategory::InvalidInput,
-            "where_clauses must be an object",
-        )
-    })?;
-
-    let mut internal_clauses = InternalClauses::default();
-
-    // Parse primary_key_in_clause
-    if let Ok(clause) = Reflect::get(&obj, &JsValue::from_str("primary_key_in_clause")) {
-        if !clause.is_null() && !clause.is_undefined() {
-            internal_clauses.primary_key_in_clause = Some(parse_where_clause(&clause)?);
-        }
-    }
-
-    // Parse primary_key_equal_clause
-    if let Ok(clause) = Reflect::get(&obj, &JsValue::from_str("primary_key_equal_clause")) {
-        if !clause.is_null() && !clause.is_undefined() {
-            internal_clauses.primary_key_equal_clause = Some(parse_where_clause(&clause)?);
-        }
-    }
-
-    // Parse in_clause
-    if let Ok(clause) = Reflect::get(&obj, &JsValue::from_str("in_clause")) {
-        if !clause.is_null() && !clause.is_undefined() {
-            internal_clauses.in_clause = Some(parse_where_clause(&clause)?);
-        }
-    }
-
-    // Parse range_clause
-    if let Ok(clause) = Reflect::get(&obj, &JsValue::from_str("range_clause")) {
-        if !clause.is_null() && !clause.is_undefined() {
-            internal_clauses.range_clause = Some(parse_where_clause(&clause)?);
-        }
-    }
-
-    // Parse equal_clauses
-    if let Ok(clauses) = Reflect::get(&obj, &JsValue::from_str("equal_clauses")) {
-        if !clauses.is_null() && !clauses.is_undefined() {
-            let clauses_obj: Object = clauses.dyn_into().map_err(|_| {
-                format_error(
-                    ErrorCategory::InvalidInput,
-                    "equal_clauses must be an object",
-                )
-            })?;
-
-            let keys = Object::keys(&clauses_obj);
-            check_object_bounds(keys.length() as usize, "equal_clauses")?;
-            let mut equal_clauses = BTreeMap::new();
-
-            for i in 0..keys.length() {
-                let key = keys.get(i);
-                let key_str = key.as_string().ok_or_else(|| {
-                    format_error(ErrorCategory::InvalidInput, "object key must be a string")
-                })?;
-
-                let clause = Reflect::get(&clauses_obj, &key).map_err(|_| {
-                    format_error(
-                        ErrorCategory::InvalidInput,
-                        "failed to get clause from object",
-                    )
-                })?;
-
-                equal_clauses.insert(key_str, parse_where_clause(&clause)?);
-            }
-
-            internal_clauses.equal_clauses = equal_clauses;
-        }
-    }
-
-    Ok(internal_clauses)
-}
-
-fn parse_where_clause(clause_js: &JsValue) -> Result<WhereClause, JsValue> {
-    let obj: Object = clause_js.clone().dyn_into().map_err(|_| {
-        format_error(
-            ErrorCategory::InvalidInput,
-            "where clause must be an object",
-        )
-    })?;
-
-    let field = Reflect::get(&obj, &JsValue::from_str("field"))
-        .map_err(|_| {
-            format_error(
-                ErrorCategory::InvalidInput,
-                "where clause missing field property",
-            )
-        })?
-        .as_string()
-        .ok_or_else(|| format_error(ErrorCategory::InvalidInput, "field must be a string"))?;
-
-    let operator_str = Reflect::get(&obj, &JsValue::from_str("operator"))
-        .map_err(|_| {
-            format_error(
-                ErrorCategory::InvalidInput,
-                "where clause missing operator property",
-            )
-        })?
-        .as_string()
-        .ok_or_else(|| format_error(ErrorCategory::InvalidInput, "operator must be a string"))?;
-
-    let operator = match operator_str.as_str() {
-        "Equal" => WhereOperator::Equal,
-        "GreaterThan" => WhereOperator::GreaterThan,
-        "GreaterThanOrEquals" => WhereOperator::GreaterThanOrEquals,
-        "LessThan" => WhereOperator::LessThan,
-        "LessThanOrEquals" => WhereOperator::LessThanOrEquals,
-        "Between" => WhereOperator::Between,
-        "BetweenExcludeBounds" => WhereOperator::BetweenExcludeBounds,
-        "BetweenExcludeLeft" => WhereOperator::BetweenExcludeLeft,
-        "BetweenExcludeRight" => WhereOperator::BetweenExcludeRight,
-        "In" => WhereOperator::In,
-        "StartsWith" => WhereOperator::StartsWith,
-        _ => {
-            return Err(format_error_with_context(
-                ErrorCategory::InvalidInput,
-                "operator",
-                &operator_str,
-            ))
-        }
-    };
-
-    let value_js = Reflect::get(&obj, &JsValue::from_str("value")).map_err(|_| {
-        format_error(
-            ErrorCategory::InvalidInput,
-            "where clause missing value property",
-        )
-    })?;
-
-    // Check bounds if value is an array
-    if let Some(array) = value_js.dyn_ref::<js_sys::Array>() {
-        check_array_bounds(array.length() as usize, "where clause value")?;
-    }
-
-    let value: Value = from_value(value_js)
-        .map_err(|e| format_result_error(ErrorCategory::DeserializationError, e))?;
-
-    Ok(WhereClause {
-        field,
-        operator,
-        value,
-    })
-}
-
-fn parse_order_by(order_by_js: &JsValue) -> Result<IndexMap<String, OrderClause>, JsValue> {
-    let mut order_by_map = IndexMap::new();
-
-    if order_by_js.is_null() || order_by_js.is_undefined() {
-        return Ok(order_by_map);
-    }
-
-    let obj: Object = order_by_js
-        .clone()
-        .dyn_into()
-        .map_err(|_| format_error(ErrorCategory::InvalidInput, "order_by must be an object"))?;
-
-    let keys = Object::keys(&obj);
-
-    for i in 0..keys.length() {
-        let key = keys.get(i);
-        let key_str = key.as_string().ok_or_else(|| {
-            format_error(ErrorCategory::InvalidInput, "object key must be a string")
-        })?;
-
-        let clause_js = Reflect::get(&obj, &key)
-            .map_err(|_| format_error(ErrorCategory::InvalidInput, "failed to get order clause"))?;
-
-        let clause_obj: Object = clause_js.dyn_into().map_err(|_| {
-            format_error(
-                ErrorCategory::InvalidInput,
-                "order clause must be an object",
-            )
-        })?;
-
-        let field = Reflect::get(&clause_obj, &JsValue::from_str("field"))
-            .map_err(|_| {
-                format_error(
-                    ErrorCategory::InvalidInput,
-                    "order clause missing field property",
-                )
-            })?
-            .as_string()
-            .ok_or_else(|| format_error(ErrorCategory::InvalidInput, "field must be a string"))?;
-
-        let ascending = Reflect::get(&clause_obj, &JsValue::from_str("ascending"))
-            .map_err(|_| {
-                format_error(
-                    ErrorCategory::InvalidInput,
-                    "order clause missing ascending property",
-                )
-            })?
-            .as_bool()
-            .ok_or_else(|| {
-                format_error(ErrorCategory::InvalidInput, "ascending must be a boolean")
-            })?;
-
-        order_by_map.insert(key_str, OrderClause { field, ascending });
-    }
-
-    Ok(order_by_map)
 }
